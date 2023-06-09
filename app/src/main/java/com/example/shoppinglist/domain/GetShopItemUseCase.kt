037f5d125f5565ac6c34162b70b03a6e27e15992
Kirill Pomaskin <kirill.pomaskin@gmail.com>
2023-06-10 01:33:31 +0300

    Creating data layer (the simplest implementation) It needs a database in the future

--- app/src/main/java/com/example/shoppinglist/domain/GetShopItemUseCase.kt
+++ app/src/main/java/com/example/shoppinglist/domain/GetShopItemUseCase.kt
@@ -2,7 +2,7 @@ package com.example.shoppinglist.domain
 
 class GetShopItemUseCase(private val shopListRepository: ShopListRepository) {
 
-    fun getShopItem(id: Int): ShopItem {
-        return shopListRepository.getShopItem(id)
+    fun getShopItem(shopItemId: Int): ShopItem {
+        return shopListRepository.getShopItem(shopItemId)
     }
 }
